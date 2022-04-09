94a60622e434dc240f86ff44ac1d17689d6f65c5
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2022-04-08 21:44:12 -0500

    fixing errors
    arreglando errores

--- main.py
+++ main.py
@@ -2,66 +2,54 @@ from random import choice
 from src.neural_network import NeuralNetwork
 
 train=[
+
     {
         'input':[0,0],
         'target':[0]
-    },
-    {
-        "input":[0,1],
-        "target":[1]
-    },
-    {
-        "input":[1,0],
-        "target":[1]
-
-    },
-    {
-        "input":[1,0],
-        "target":[1]
-
-    },
-    {
-        "input":[1,1],
-        "target":[0]
     },
      {
-        'input':[0,0],
-        'target':[0]
+        "input":[1,1],
+        "target":[0]
     },
     {
+        "input":[1,0],
+        "target":[1]
+    },
+     
+      {
         "input":[0,1],
         "target":[1]
     },
- 
-    {
-        "input":[1,1],
-        "target":[0]
-    }
+  
 ]
+def test(nen):
+    
+    print("it should be near to 1     ",nen.predict([1,0]),nen.cost([1,0],[1]))
+      
+    print("the same as before,1       ",nen.predict([0,1]),nen.cost([0,1],[1]))
+  
+    print("it should be near to 0     ",nen.predict([1,1]),nen.cost([1,1],[0]))
+    
+    print("the same as before, nothing",nen.predict([0,0]),nen.cost([0,0],[0]))
+
+   
 def main():
+
    
    
-    nen=NeuralNetwork(2,1,[2,4])
+    nen=NeuralNetwork(2,1,[2])
 
   
-    print(nen.feed_foward([0,0]))
-    print(nen.weights)
-    for i in range(10000):
+    test(nen)
+ 
+ 
+    for i in range(6000):
         t=choice(train)
         nen.train(t['input'],t['target'])
-        nen.clear_nn()
-       
+        nen.clear_nn()  
         
-    nen.clear_nn()
-    print(nen.nn,nen.weights)
-    nen.clear_nn()
-    print("it should be near to 1     ",nen.feed_foward([1,0]))
-    nen.clear_nn()
-    print("the same as before,1       ",nen.feed_foward([0,1]))
-    nen.clear_nn()
-    print("it should be near to 0     ",nen.feed_foward([1,1]))
-    nen.clear_nn()
-    print("the same as before, nothing",nen.feed_foward([0,0]))
+    test(nen)
+
  
 if __name__ == "__main__":
     main()
--- src/neural_network.py
+++ src/neural_network.py
@@ -1,76 +1,58 @@
-from math import exp
+from html.entities import name2codepoint
+
 
 from operator import index
 from random import random
-from re import template
-
+from re import T, template
+from tkinter import W
+import numpy as np 
 #https://stackabuse.com/python-how-to-flatten-list-of-lists/
 flatten_list = lambda irregular_list:[element for item in irregular_list for element in flatten_list(item)] if type(irregular_list) is list else [irregular_list]
-sigmoid=lambda x: 1/(1+exp(-x))
-dsigmoid=lambda y:y * (1 - y)
-        
+sigmoid= lambda x: 1/(1+np.exp(-x))
+dsigmoid=np.vectorize(lambda y:y * (1 - y))
+
 
 class NeuralNetwork():
 
     def __init__(self,length_of_input,length_of_output,hidden_layers=[2]):
-        nn=[]
-        hidden_nodes=flatten_list(hidden_layers)
-        template_nn=[]
-        weights=[]    
-        bias=[]
-        for length in [length_of_input]+hidden_nodes+[length_of_output]:
-            nn.append([])
-            
-            bias.append([random()]*length)
-            template_nn.append([])
-            template_nn[-1]=[0]*length
-            nn[-1]=[0]*length
-
-            
-
-
-        for l in range(len(nn)-1):
-            weights.append([])
-
-            for _ in range(len(nn[l])):
-                weights[-1].append([random()]*len(nn[l+1]))
-
-        bias[0]=0 
         
-        self.nn=nn
-        self.weights=weights
-        self.bias=bias
-        self.learning_rate=1
+        
+       
+        bias=[]
+        weights=[]
+        hidden_nodes=flatten_list(hidden_layers)
+        
+        layers=[length_of_input]+hidden_nodes+[length_of_output]
+        nn=[[]]*len(layers)
+        for l in range(len(layers)-1):
+            bias.append(np.random.rand(layers[l+1],1))
+            weights.append(np.random.rand(layers[l+1],layers[l]))
+     
+        bias[0]=np.zeros((length_of_input,1))
+        self.nn=(nn)
+        self.weights=(weights)
+        self.bias=(bias)
+        self.learning_rate=2
+
         
 
   
     
 
     def feed_foward(self,input):
-    
+       
         nn=self.nn
-        if len(nn[0])!=len(input):
-            raise ValueError("Inputs and NN inputs are not the same length")
+        
+        
         weights=self.weights
         bias=self.bias
-        
-        nn[0]=input
-        for l in range(len(nn)):
-            for n in range(len(nn[l])):
-                if l!=0:     
-                  
-                    nn[l][n]+=bias[l][n]
-                   
-                    nn[l][n]=sigmoid(nn[l][n])
-                if l==len(nn)-1:continue
-                for w in range(len(weights[l][n])):
-                    
-                    nn[l+1][w]+=nn[l][n]*weights[l][n][w]
-           
+        nn[0]=np.matrix(input)
+        for l in range(len(nn)-1):
+            nn_t=np.reshape(nn[l],(-1,1))
+            nn[l+1]=sigmoid(np.matmul(weights[l],nn_t)+bias[l])
+        self.nn=nn  
 
-        output=nn[-1]
-       
-        return output
+        return nn[-1]
 
 
 
@@ -78,111 +60,43 @@ class NeuralNetwork():
     def clear_nn(self)->None:
         for l in range(len(self.nn)):
 
-            self.nn[l]=[0]*len(self.nn[l])
-    def transpose(self,matrix):
-        if type(matrix[0]) is not list:
-            matrix=list(map(lambda x:[]+[x],matrix))
-        out = list(map(list,zip(*matrix)))     
-        return out
-    def multiplyOne(self,a,b):
-       
-
-        if type(a[0]) !=  list:
-            a=list(map(lambda x:[]+[x],a))
-        if type(b[0]) !=  list:
-            b=list(map(lambda x:[]+[x],b))
-     
-        out=[[0]*len(b[0])]*len(a) 
-        for i in range( len(out)):
-            for j in range( len(out[0])):
-                for k in range(len(a[0])):
-                
-                    out[i][j]+=(a[i][k]*b[k][j])
-        return out
-    def multiply(self,a,b):
-        
-        out=[]
-        
-        if type(b) == list:
-            if type(a[0]) !=  list:
-                a=list(map(lambda x:[]+[x],a))
-            if type(b[0]) !=  list:
-                b=list(map(lambda x:[]+[x],b))
-            out=a
-            for i in range( len(a)):
-                for j in range( len(a[0])):
-                    out[i][j]*=b[i][j]
-        else:
-           
-            if type(a[0]) != list:
-                a=list(map(lambda x:[]+[x],a))
-            out=a
-          
-            for i in range( len(a)):
-            
-                for j in range( len(a[0])):
-                    
-                    out[i][j]*=b
-            
-       
-        return out
-    def add(self,a,n):
-       
-      
-        out=[]
-        if type(n)is list:
-            if type(n[0]) is not list:
-                n=list(map(lambda x:[]+[x],n))
-
-            if type(a[0]) !=  list:
-                a=list(map(lambda x:[]+[x],a))
-            out=a
-            for i in range(len(a)):
-                
-                for j in range(len(a[0])):
-                 
-                    out[i][j] += n[i][j];
-                
-        
-        return out
-    
+            self.nn[l]=np.zeros(len(self.nn[l]))
+   
   
     def cost(self,input,target):
-       
+        
+        output=self.predict(input)
+        return np.sum(np.square(output-target))
+    def predict(self,input):
         output=self.feed_foward(input)
         self.clear_nn()
-        return sum(map(lambda x:(x[0]-x[1])**2,zip(output,target)))
-        
+        return output   
     def train(self,input,target):
         
         output=self.feed_foward(input)
-        
-        errors=list(map(lambda x:x[0]-x[1],zip(target,output)))
-   
+       
+        errors=np.matrix(np.subtract(target,output))
      
-        gradient=list(map(dsigmoid,output))
-        gradient=self.multiply(gradient,errors)
-        gradient=self.multiply(gradient,self.learning_rate)
-        
+        gradient=(dsigmoid(output))
+        gradient=np.multiply(gradient,errors)
+        gradient=np.multiply(gradient,self.learning_rate)
         l=len(self.nn)-2
         while l>=0:
-        
-            
-            layer_t=self.transpose(self.nn[l])
-            deltGrad=self.multiplyOne(gradient,layer_t)
-            
 
-            self.weights[l]=self.add(self.weights[l],self.transpose(deltGrad))
-            self.bias[l+1]=flatten_list(self.add(self.bias[l+1],gradient))
+            nn_t=np.transpose(self.nn[l])
+            #print("pos",l)
+            deltGrad= np.multiply( gradient,nn_t)
+            #print("weights before",l,self.weights[l])
+            self.weights[l]=np.add(self.weights[l], deltGrad)
+           # print("weights after",l,self.weights[l])
+          
+            self.bias[l]=np.add(self.bias[l],gradient)
             if l==0:break   
-            weight_t=self.transpose(self.weights[l])  
-            errors=flatten_list(self.multiplyOne(errors,weight_t))
-            
-            gradient=list(map(dsigmoid,self.nn[l]))
-           
-            gradient=self.multiply(gradient,errors)
-           
-            gradient=self.multiply(gradient,self.learning_rate)
+            weight_t=np.transpose(self.weights[l])
+            errors=np.matmul(weight_t,errors)
+            gradient=dsigmoid(self.nn[l])
+            gradient=np.multiply(gradient,errors)
+            gradient=np.multiply(gradient,self.learning_rate)
             l-=1
         self.clear_nn()
 
