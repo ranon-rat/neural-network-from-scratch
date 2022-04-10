6c683569f624b94e197277bd29057ecf10052d45
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2022-04-10 02:18:24 -0500

    now it save the neural network structure

--- main.py
+++ main.py
@@ -1,43 +1,15 @@
-
-from random import choice
 from src.neural_network import NeuralNetwork
-from matplotlib import pyplot as plt
+from json import loads
+nen=NeuralNetwork(1,1,[0])
 
-from celluloid import Camera
-
-
-plt.style.use('dark_background')
-
-fig=plt.figure()
-camera=Camera(fig)
-nen=NeuralNetwork(2,1,[2],min_lr=1e-3,max_lr=1e-1,decay_factor=0.99)
-
-train=[
-    {
-        'input':[0,0],
-        'target':[0]
-    },
-     {
-        "input":[1,1],
-        "target":[0]
-    },
-    {
-        "input":[1,0],
-        "target":[1]
-    },
-      {
-        "input":[0,1],
-        "target":[1]
-    }, 
-]
 
 testing=1
 
 
-def test(nen):
-    global testing,train
+def test(nen,data):
+    global testing
     print(("#"*10),"TESTING #",testing,("#"*10))
-    for t in train:
+    for t in data:
         print("\nTESTING",t['input'],"TARGET",t['target'])
         print("\ninput:",t['input'],"target:",t['target'])
         cost,output=nen.cost(t['input'],t['target'])
@@ -45,41 +17,15 @@ def test(nen):
         print("cost:",cost,"\n")
         print("-"*20)
     
-   
     testing+=1
-    print("GENERAL COST:",nen.general_cost(train))
-    
+    print("GENERAL COST:",nen.general_cost(data))
+
 def main():
-    test(nen)
-    epochs=1000
-    y=[0]*(epochs)
+
+    data=loads(open("data.json").read())
+    nen.copy_from("nn.json")
+
+    test(nen,data)
     
-
-    for i in range(epochs):
-        t=choice(train)
-        nen.train(epochs,i,t['input'],t['target'])
-       
-        cost=nen.general_cost(train)
-        #=nen.general_cost(train)
-        y[i]=(cost)
-    plt.plot(y)
-    test(nen)
-
-       
-    plt.show()
-        
-       
-     #   camera.snap()
-    #ani=camera.animate()
-    #ani.save('animation.gif', writer='ffmpeg', fps=35)
-    
-
-    
-
-    
-   
-
-
- 
 if __name__ == "__main__":
     main()
--- src/neural_network.py
+++ src/neural_network.py
@@ -1,34 +1,52 @@
-from math import atan
 import numpy as np 
+from json import dumps,loads
+from time import time
 #https://stackabuse.com/python-how-to-flatten-list-of-lists/
 
-sigmoid= np.vectorize(lambda x: 1/(1+np.exp(-x)))
-dsigmoid=np.vectorize(lambda y:y * (1 - y))
-relu=np.vectorize(lambda x: max(0,x))
-drelu=np.vectorize(lambda x:np.where(x>0,1,0))
-tanh=np.vectorize(lambda x:np.tanh(x))
-dtanh=np.vectorize(lambda y:1-(y**2))
+
+
+tanh={
+    "activation":np.vectorize(lambda x:np.tanh(x)),
+    "derivate":np.vectorize(lambda y:1-(y**2))
+}
+sigmoid={
+    "activation":np.vectorize(lambda x:1/(1+np.exp(-x))),
+    "derivate":np.vectorize(lambda y:y * (1 - y))
+}
+    
+relu={
+    "activation":np.vectorize(lambda x:max(0,x)),
+    "derivate":np.vectorize(lambda y:np.where(y>0,1,0))
+}
+activation_functions={
+    "tanh":tanh,
+    "relu":relu,
+    "sigmoid":sigmoid
+
+}
+
+
 class NeuralNetwork():
 
-    def __init__(self,length_of_input,length_of_output,hidden_layers=[2], min_lr=1e-5,max_lr=1e-2,decay_factor=0.95,step_size=10,cycle_size=10):
+    def __init__(self,length_of_input:int,length_of_output:int,hidden_layers=[2], min_lr=1e-5,max_lr=1e-2,decay_factor=0.95,step_size=10,cycle_size=10):
         
-        layers=[length_of_input]+hidden_layers+[length_of_output]
-        funcs=[tanh]*len(hidden_layers)+[tanh]
-        dfuncs=[dtanh]*len(hidden_layers)+[dtanh]
-       
+        layers_length=[length_of_input]+hidden_layers+[length_of_output]
         bias=[]
-        weights=[]
-       
-        nn=[[]]*len(layers)
-    
-        for l in range(len(layers)-1):
-            bias.append(np.matrix(np.random.rand(layers[l+1],1)))
-            weights.append(np.matrix(np.random.rand(layers[l+1],layers[l])))
-      
-        self.derivate_funcs=dfuncs
+        weights=[]       
+        
+        funcs=[relu]*len(hidden_layers)+[tanh]       
+        layers=[[]]*len(layers_length)
+        
+        for l in range(len(layers_length)-1):
+            bias.append(np.matrix(np.random.rand(layers_length[l+1],1)))
+            weights.append(np.matrix(np.random.rand(layers_length[l+1],layers_length[l])))
+
+        self.activation_funcs_names=["relu"]*len(hidden_layers)+["tanh"]
         self.activation_funcs=funcs
-       
-        self.nn=(nn)
+        
+        self.layers_length=len(layers_length)
+        self.layers=layers
+        
         self.weights=(weights)
         self.bias=(bias)
         
@@ -40,83 +58,123 @@ class NeuralNetwork():
         self.step_size=step_size
         self.batch_size=0
         self.next_restart=cycle_size
+    def copy_structure(self,nn:dict)->None:
+        self.max_lr=nn["max_learning_rate"]
+        self.min_lr=nn["min_learning_rate"]
+        
+        self.activation_funcs_names=nn["activation_funcs"]
+        #im just using the names to access the functions
+        self.activation_funcs=list(map(lambda x:activation_functions[x],nn["activation_funcs"]))
+        # i make the same thing that i make when i define the class
+        self.layers_length=nn["layers_amount"]
+        self.layers=[[]]*nn["layers_amount"]
+        # i just copy the weights and bias
+        self.weights=list(map(np.matrix,nn["weights"]))
+        self.bias=list(map(np.matrix,nn["bias"]))
+    
+    def give_structure(self)->dict:
+        return {  
+            "max_learning_rate":self.max_lr,
+            "min_learning_rate":self.min_lr,
+            "activation_funcs":self.activation_funcs_names,
+            "layers_amount":self.layers_length,
+            "weights":list(map(lambda x:x.tolist(), self.weights)),
+            "bias":list(map(lambda x:x.tolist(), self.bias)),
+           
+        }
+    def copy_from(self,path:str):
+        with open(path, "r") as infile:
+            nn=loads(infile.read())
+            self.copy_structure((nn))
+        
+    def save(self,path:str="",name="neural_network"+str(time())+".json")->None:
+        with open(path+name, "w") as outfile:
+            s=dumps(self.give_structure(),indent = 4)
+            outfile.write(s)
 
-
-    def feed_foward(self,input):
-        nn=self.nn 
+    def feed_foward(self,input:list)->np.matrix:
+        layers=self.layers 
         weights=self.weights
         bias=self.bias
-        nn[0]=np.matrix(input).transpose()
-        for l in range(len(nn)-1):
+        layers[0]=np.matrix(input).transpose()
+
+        for l in range(len(layers)-1):
             
             """
-         
-            idk how to explain myself so , here is a good resource 
             http://matrixmultiplication.xyz/
             """
         
-            res=np.matmul(weights[l],nn[l])
-            nn[l+1]=self.activation_funcs[l](res+bias[l])
+            res=np.matmul(weights[l],layers[l])
+            layers[l+1]=self.activation_funcs[l]["activation"](res+bias[l])
         
-        self.nn=nn
-        return nn[-1]
+        self.layers=layers
+        return layers[-1]
 
 
 
       
     
-    def general_cost(self,train):
-       
-      
+    def general_cost(self,train:list)->float:
+        # this is for getting the general cost
+        # its ugly
         cost=sum(map(lambda x:self.cost(x["input"],x["target"])[0] ,train))
         return cost/len(train)
-    def cost(self,input,target):
+    def cost(self,input:float,target:float)->float:
+
         output=self.predict(input)
-     
         return (np.sum(np.square(output-target))/len(target),output)
 
-    def predict(self,input):
+    def predict(self,input)->float:
+
         output=self.feed_foward(input)
-   
         return output   
     #https://www.jeremyjordan.me/nn-learning-rate/
-    def update_learning_rate(self):
+    def update_learning_rate(self)->None:
         x = self.batch_size/(self.step_size*self.next_restart)
         self.learning_rate=self.min_lr+0.5*(self.max_lr-self.min_lr)*(1+np.cos(x*np.pi))
  
     
 
-    def train(self,iterations:int,iteration,target,input):
+    def train(self,iteration,input:float,target:float,)->None:
+        # this is for updating the learning rate
+        # with this the probability of getting stuck in local minima
+        # is reduced
         self.update_learning_rate()
-        self.backprop(target,input)
+
+        self.backprop(input,target)
         self.batch_size+=1
+
+
         if iteration%self.step_size==0:
             self.batch_size=0
             self.next_restart+=self.step_size
+            #self.max_lr*=self.decay_factor
 
-    def gradient_descent(self,output,errors,derivate_func):
+    def gradient_descent(self,output:np.matrix,errors:np.matrix,derivate_func)->np.matrix:
         return np.multiply( np.multiply(errors,derivate_func(output)),self.learning_rate)
        
-    def backprop(self,input,target):
+    def backprop(self,input:float,target:float):
         
         output=self.feed_foward(input)
         errors=np.subtract(target,output)
-        gradient=self.gradient_descent(output,errors,self.derivate_funcs[-1])
+        gradient=self.gradient_descent(output,errors,self.activation_funcs[-1]["derivate"])
        
-        for l in range(len(self.nn)-1)[::-1]:
+        for l in range(len(self.layers)-1)[::-1]:
           
-            nn_t=np.transpose(self.nn[l])
-            deltgrad=np.matmul(gradient,nn_t)
+            layers_t=np.transpose(self.layers[l])
+            deltgrad=np.matmul(gradient,layers_t)
             # this is for getting the weights.. you know what i mean  
              # its the delta of the gradient so 
             # it will reduce the errors
             
             self.weights[l]=np.add(self.weights[l],deltgrad)
             self.bias[l]=np.add(self.bias[l],gradient)
+            
             if l==0:break    
+
             weight_t=(self.weights[l]).transpose()
             errors=np.matmul(weight_t,errors)
-            gradient=self.gradient_descent(self.nn[l],errors,self.derivate_funcs[l-1])
+            gradient=self.gradient_descent(self.layers[l],errors,self.activation_funcs[l-1]["derivate"])
         
 
 
