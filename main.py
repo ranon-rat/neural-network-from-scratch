46441859b2b2e6d758cc0320070373700a1b068b
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2022-04-09 21:52:57 -0500

    multilayer support

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+
 from random import choice
 from src.neural_network import NeuralNetwork
+from matplotlib import pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
+from celluloid import Camera
+
+
+plt.style.use('dark_background')
+
+fig=plt.figure()
+camera=Camera(fig)
+nen=NeuralNetwork(2,1,[2])
+
 train=[
     {
         'input':[0,0],
@@ -18,7 +31,10 @@ train=[
         "target":[1]
     }, 
 ]
+
 testing=1
+
+
 def test(nen):
     global testing,train
     print(("-"*10),"TESTING #",testing,("-"*10))
@@ -30,25 +46,39 @@ def test(nen):
         print("\tcost:",cost)
     
    
-   
     testing+=1
-   
+    print("GENERAL COST:",nen.general_cost(train))
+    print(nen.nn)
 def main():
-
-   
-   
-    nen=NeuralNetwork(2,1,[2])
-
-  
     test(nen)
- 
- 
-    for i in range(6000):
+    epochs=6000
+    y=[0]*(epochs//10)
+    
+
+    for i in range(epochs):
         t=choice(train)
         nen.train(t['input'],t['target'])
-        nen.clear_nn()  
         
+        if i%10==0: 
+            cost,_=nen.cost(t['input'],t['target'])
+            y[i//10]=(cost)
+    plt.plot(y)
     test(nen)
+
+       
+    plt.show()
+        
+       
+     #   camera.snap()
+    #ani=camera.animate()
+    #ani.save('animation.gif', writer='ffmpeg', fps=35)
+    
+
+    
+
+    
+   
+
 
  
 if __name__ == "__main__":
--- src/neural_network.py
+++ src/neural_network.py
@@ -1,37 +1,44 @@
 
+import random
+from turtle import clear
 import numpy as np 
 #https://stackabuse.com/python-how-to-flatten-list-of-lists/
-flatten_list = lambda irregular_list:[element for item in irregular_list for element in flatten_list(item)] if type(irregular_list) is list else [irregular_list]
-sigmoid= lambda x: 1/(1+np.exp(-x))
-dsigmoid=np.vectorize(lambda y:y * (1 - y))
 
+sigmoid= np.vectorize(lambda x: 1/(1+np.exp(-x)))
+dsigmoid=np.vectorize(lambda y:y * (1 - y))
+relu=np.vectorize(lambda x: max(0,x))
+drelu=lambda x:np.where(x>0,1,0)
+
+tanh=np.vectorize(lambda x:np.tanh(x))
+dtanh=np.vectorize(lambda y:1-(y**2))
 
 class NeuralNetwork():
 
     def __init__(self,length_of_input,length_of_output,hidden_layers=[2]):
         layers=[length_of_input]+hidden_layers+[length_of_output]
+        funcs=[tanh]*len(hidden_layers)+[tanh]
+        dfuncs=[dtanh]*len(hidden_layers)+[dtanh]
         bias=[]
         weights=[]
         nn=[[]]*len(layers)
+    
         for l in range(len(layers)-1):
+          
             bias.append(np.matrix(np.random.rand(layers[l+1],1)))
             weights.append(np.matrix(np.random.rand(layers[l+1],layers[l])))
-       
+      
+        self.derivate_funcs=dfuncs
+        self.activation_funcs=funcs
         self.nn=(nn)
         self.weights=(weights)
         self.bias=(bias)
-        self.learning_rate=4
+        self.learning_rate=1/(10**(len(hidden_layers))-1)#1/(10**(len(hidden_layers)-1))
 
     def feed_foward(self,input):
         nn=self.nn 
         weights=self.weights
         bias=self.bias
-        """
-        for some reason , after i convert the input to a matrix
-        it returns me [[x],[x]] instead of
-        """
         nn[0]=np.matrix(input).transpose()
-      
         for l in range(len(nn)-1):
             
             """
@@ -39,68 +46,60 @@ class NeuralNetwork():
             idk how to explain myself so , here is a good resource 
             http://matrixmultiplication.xyz/
             """
-            nn_t=nn[l]
-            res=np.matmul(weights[l],nn_t)
-            nn[l+1]=sigmoid(res+bias[l])
-      
-        output=nn[-1]
+        
+            res=np.matmul(weights[l],nn[l])
+           
+            nn[l+1]=self.activation_funcs[l](res+bias[l])
+        
         self.nn=nn
-        
-        
-        return output
+        return nn[-1]
 
 
 
       
-    def clear_nn(self)->None: #yes , i hate this too
-        for l in range(len(self.nn)):
-            self.nn[l]-=self.nn[l]
-  
+    
+    def general_cost(self,train):
+       
+      
+        cost=sum(map(lambda x:self.cost(x["input"],x["target"])[0] ,train))
+        return cost
     def cost(self,input,target):
         output=self.predict(input)
-        return (np.sum(np.square(output-target)),output)
+     
+        return (np.sum(np.square(output-target))/len(target),output)
 
     def predict(self,input):
         output=self.feed_foward(input)
+   
         return output   
-    def gradient_descent(self,output,errors):
-        gradient=(dsigmoid(output))
-        # then i need to multiply it for gettint the erros and with that
-        # i will know how to reduce them
-
+    def gradient_descent(self,output,errors,derivate_func):
+        gradient=derivate_func(output)
         gradient=np.multiply(gradient,errors)
-        # the learning rate its just for the speed of the training
-        # maybe it could be more inexact and have more errors so
-        # if you wanted you can change it
-        # but maybe it will be in a point where it would be less presice
-        # local minima
         gradient=np.multiply(gradient,self.learning_rate)
         return (gradient)
+   
+  
     def train(self,input,target):
         
         output=self.feed_foward(input)
+        errors=np.subtract(target,output)
+        gradient=self.gradient_descent(output,errors,self.derivate_funcs[-1])
        
-        errors=(np.subtract(target,output))
-        gradient=self.gradient_descent(output,errors)
-        l=len(self.nn)-2
-        while l>=0:
-            #print("gradient",gradient,self.nn[l])
+        for l in range(len(self.nn)-1)[::-1]:
+          
             nn_t=np.transpose(self.nn[l])
           
-            # this is for getting the weights.. you know what i mean
-            deltGrad=( np.matmul(gradient,nn_t))
-            # its the delta of the gradient so 
+            # this is for getting the weights.. you know what i mean  
+             # its the delta of the gradient so 
             # it will reduce the errors
-         
-            self.weights[l]=np.add(self.weights[l], deltGrad)
-           
+            
+            self.weights[l]=np.add(self.weights[l],np.matmul(gradient,nn_t))
             self.bias[l]=np.add(self.bias[l],gradient)
-            if l==0:break
+            if l==0:break    
+            weight_t=(self.weights[l]).transpose()
+            errors=np.matmul(weight_t,errors)
+            gradient=self.gradient_descent(self.nn[l],errors,self.derivate_funcs[l-1])
         
-            weight_t=np.transpose(self.weights[l])
-            errors=np.matmul(weight_t,gradient)
-            gradient=self.gradient_descent(self.nn[l],errors)
-            l-=1
-        self.clear_nn()
+
 
       
