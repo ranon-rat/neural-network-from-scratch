cd74cfacb21c9e73f6a5e8b9e1fd42221206cfe6
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2022-04-11 22:12:29 -0500

    delete this trash

--- main.py
+++ main.py
@@ -2,74 +2,7 @@ import numpy as np
 from random import choice, shuffle
 from src.neural_network import NeuralNetwork
 import matplotlib.pyplot as plt
-testing=1
-fig, ax = plt.subplots(1,1)
-nen=NeuralNetwork(784,10,hidden_layers=[16,16],learning_rate=0.0007)
-LEARNING_RATES = [0.025, 0.0025, 2.5]
-def test_one(nen,target,input):
-    
-    
-    print("|input:",input[:4],"...","target:",target)
-    loss,output=nen.loss(target,input)
-    print("predictions")
-    for i in range(len(output)):
-        print(i,":",output[i])
-   
-    print("|loss:",loss,"\n")
-    print("-"*20)
-def test(nen:NeuralNetwork,data):
-    a=[]
-    for i in data:
-        target=[0]*10
-        target[int(i[0])]= 1
-        
-        input=np.divide(i[1:],255)
-      
-        a.append({
-            "target":target,
-            "input":input
-        })
-    
-    c=nen.gen_loss(a)
-    return c
-
-#    print("How exact it is?:",nen.exact(data))
-def open_csv(path:str):
-    data=np.loadtxt(path,delimiter=",")
-    return data
 def main():
-
-    training_data=open_csv("data/mnist_train.csv")
-    testing_data=open_csv("data/mnist_test.csv")
- 
-    
-
-    c=[0]*30
-  
-    for i in range(30):
-        shuffle(training_data)
-        mini_batches = [
-               training_data[k:k+10]
-               for k in range(0, len(training_data), 10)]
-        for m in mini_batches:
-            for t in  m:
-                target=[0]*10
-                target[int(t[0])]= 1
-                input=np.divide(t[1:],255)
-                nen.train(target,input) 
-        c[i]=test(nen,testing_data)
-        print(c[i])
-        
-        
-#
-    
-    plt.plot(c)
-    for t in testing_data:
-        target=[0]*10
-        target[int(t[0])]= 1
-        input=np.divide(t[1:],255)
-        print("we want",t[0])
-        test_one(nen,target,input)
 
     plt.show()
 if __name__ == "__main__":
--- src/neural_network.py
+++ src/neural_network.py
@@ -3,10 +3,14 @@ from json import dumps,loads
 from time import time
 #https://stackabuse.com/python-how-to-flatten-list-of-lists/
 
-
+def one_hot(Y):
+    one_hot_Y = np.zeros((Y.size, Y.max() + 1))
+    one_hot_Y[np.arange(Y.size), Y] = 1
+    one_hot_Y = one_hot_Y.T
+    return one_hot_Y
 
 tanh={
-    "activation":(lambda x:np.tanh(x)),
+    "activation":np.vectorize(lambda x:np.tanh(x)),
     "derivate":np.vectorize(lambda y:1-(y**2))
 }
 
@@ -17,14 +21,23 @@ sigmoid={
     
 relu={
     "activation":np.vectorize(lambda x:max(0,x)),
-    "derivate":lambda y:np.where(y>0,1,0)
+    "derivate":np.vectorize(lambda y:np.where(y>0,1,0))
+}
+
+soft_max={
+    "activation":np.vectorize(lambda x:np.exp(x)/np.sum(np.exp(x))),
+    "derivate":np.vectorize(lambda y:0 - (y[0] * y[1]))
 }
 activation_functions={
     "tanh":tanh,
     "relu":relu,
-    "sigmoid":sigmoid
-
+    "sigmoid":sigmoid,
+    "soft_max":soft_max,
 }
+
+
+
+
 
 
 class NeuralNetwork():
@@ -36,19 +49,18 @@ class NeuralNetwork():
         weights=[]       
         
         funcs=[relu]*len(hidden_layers)+[sigmoid]       
-        layers=[[]]*len(layers_length)
+       
         
         for l in range(len(layers_length)-1):
-            bias.append(np.matrix(np.random.rand(layers_length[l+1],1)))
-            weights.append(np.matrix(np.random.rand(layers_length[l+1],layers_length[l])))
+            bias.append((np.random.rand(10,1))-0.5)
+            weights.append((np.random.rand(10,layers_length[l]))-0.5)
 
         self.activation_funcs_names=["relu"]*len(hidden_layers)+["sigmoid"]
+        
         self.activation_funcs=funcs
         
-        self.layers_length=len(layers_length)
-        
-        self.layers=layers
-
+        self.layers_amount=len(layers_length)
+       
         self.weights=(weights)
         self.bias=(bias)
 
@@ -59,23 +71,22 @@ class NeuralNetwork():
     def copy_structure(self,nn:dict)->None:
        
         
-        self.activation_funcs_names=nn["activation_funcs"]
+        self.activation_funcs_names=nn["activation_funcs"].copy()
         #im just using the names to access the functions
-        self.activation_funcs=list(map(lambda x:activation_functions[x],nn["activation_funcs"]))
+        self.activation_funcs=list(map(lambda x:activation_functions[x],nn["activation_funcs"])).copy()
         # i make the same thing that i make when i define the class
-        self.layers_length=nn["layers_amount"]
+        self.layers_amount=nn["layers_amount"]
      
 
         # i just copy the weights and bias
-        self.weights=list(map(np.matrix,nn["weights"]))
-        self.bias=list(map(np.matrix,nn["bias"]))
+        self.weights=list(map(np.array,nn["weights"])).copy()
+        self.bias=list(map(np.array,nn["bias"])).copy()
     
     def give_structure(self)->dict:
         return {  
-            "max_learning_rate":self.max_lr,
-            "min_learning_rate":self.min_lr,
+          
             "activation_funcs":self.activation_funcs_names,
-            "layers_amount":self.layers_length,
+            "layers_amount":self.layers_amount,
             "weights":list(map(lambda x:x.tolist(), self.weights)),
             "bias":list(map(lambda x:x.tolist(), self.bias)),
            
@@ -90,24 +101,22 @@ class NeuralNetwork():
             s=dumps(self.give_structure(),indent = 4)
             outfile.write(s)
 
-    def predict(self,input:list)->np.matrix:
+    def feed_foward(self,input)->np.matrix:
        
-        layers=self.layers
-        weights=self.weights
-        bias=self.bias
-        layers[0]=np.matrix(input).transpose()
-        for l in range(len(layers)-1):
+        
+        layers=[0]*self.layers_amount
+        
+        layers[0]=input
+
+        for l in range(self.layers_amount-1):
             """
             http://matrixmultiplication.xyz/
             """
-            res=np.dot(weights[l],layers[l])
-            res=np.add(res,bias[l])
+            res=self.weights[l].dot(layers[l])+self.bias[l]
             layers[l+1]=self.activation_funcs[l]["activation"](res)
      
-       
-        self.layers=layers
-       
-        return layers[-1]
+          
+        return layers
 
 
 
@@ -124,53 +133,74 @@ class NeuralNetwork():
         x=np.sqrt(x)
     def loss(self,target:float,input:float)->float:
 
-        output=self.predict(input)
+        output=self.feed_foward(input)
         #its a lisp reference omg
-        return (np.sum(np.square((np.subtract(output,(target))))),output)
-
-
-
-    def train(self,target,input)->None:
-        # this is for updating the learning rate
-        # with this the probability of getting stuck in local minima
-        # is reduced
-       
-        
-    
-        self.backprop(target,input) 
-
-        
-    def clear_layers(self)->None:
-
-        self.layers=[[]]*self.layers_length
-    def gradient_descent(self,output:np.matrix,errors:np.matrix,derivate_func)->np.matrix:
-        gradient=derivate_func(output)
-        gradient=np.multiply(gradient,errors)
-        gradient=(np.multiply(gradient,self.learning_rate))
-        return gradient
-       
-    def backprop(self,target:float,input:float):
-        
-        output=self.predict(input)
         target=np.matrix(target).transpose()
-        errors=np.subtract(target,output)        
-        gradient=self.gradient_descent(output,errors,self.activation_funcs[-1]["derivate"])
+        return (np.sum(np.abs(output-target)),output)
+    #just for testing
+    def get_predictions(self,A2):
+        return np.argmax(A2, 0)
+    def prediction(self,x):
+        l=self.feed_foward(x)
+        out=l[-1]
+        return self.get_predictions(out)
+    # more info
+    def get_accuracy(self,predictions, Y):
+        return np.sum(predictions == Y) / Y.size
+    
+    def update(self,dw,db):
+        for i in range(len(self.weights)):
+            self.weights[i]=(self.weights[i ]-(dw[i]*self.learning_rate))
+            self.bias[i]=   (self.bias[i]-( db[i]*self.learning_rate))
 
-        for l in reversed(range(len(self.layers)-1)):
+    def train(self,X,Y,iterations,m)->list:
+        accuracy_log=[]
+        for i in range(iterations):
+            layers=self.feed_foward(X)
+
+            dw,db=self.backprop(layers,Y,m) 
+            self.update(dw,db)
+
+            
+            if i%10==0:
+                print("_"*30,i,"_"*30)
+                
+                predictions=self.get_predictions(layers[-1])
+                accuracy=self.get_accuracy(predictions,Y)
+                accuracy_log.append(accuracy)
+                
+                
+                print("| predictions:",predictions[:10],"| accuracy:",str(accuracy)[:10],"|")
+             
+                
+        return accuracy_log
+
+    
+    
+
+    def backprop(self,layers,target,m):
+        
+        
+        one_hot_Y = one_hot(target)
+        errors=layers[-1]-one_hot_Y
+        gradient=self.activation_funcs[-1]["derivate"](layers[-1])*errors
+
+
+        dw=[0]*(len(self.weights))
+        db=[0]*(len(self.bias))
+        for l in range(len(layers)-1)[::-1]:
    
-            layers_t=(self.layers[l]).transpose()
+            layers_t=layers[l].transpose()
             deltgrad=np.dot(gradient,layers_t)
             # this is for getting the weights.. you know what i mean  
              # its the delta of the gradient so 
             # it will reduce the errors
 
-            self.weights[l] =np.add(self.weights[l],deltgrad)
-            self.bias[l]    =np.add(self.bias[l],gradient)
-            
+            dw[l] =(1 / m) *deltgrad
+            db[l] =(1 / m) *np.sum(gradient)
+
             if l==0:break    
+            errors=self.weights[l].T.dot(errors)
+            gradient=(errors*self.activation_funcs[l-1]["derivate"](layers[l]))
 
-            weight_t=(self.weights[l]).transpose()
-            errors=np.matmul(weight_t,errors)
-            gradient=self.gradient_descent(self.layers[l],errors,self.activation_funcs[l-1]["derivate"])
-     
-
+        return (dw,db)
